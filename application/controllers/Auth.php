58fd252fa2bc413cbcf6191da300154cf4f60a78
Dominik280 <domca.svoboda@email.cz>
2021-01-03 22:28:58 +0100

    another one

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -12,12 +12,14 @@ class Auth extends CI_Controller
 	public function __construct()
 	{
 		parent::__construct();
+		$this->load->model('Model');
 		$this->load->database();
 		$this->load->library(['ion_auth', 'form_validation']);
 		$this->load->helper(['url', 'language']);
 
 		$this->form_validation->set_error_delimiters($this->config->item('error_start_delimiter', 'ion_auth'), $this->config->item('error_end_delimiter', 'ion_auth'));
 		$data['polozky'] = $this->Model->vypis_menu();
+
       	$this->load->view('pages/menu', $data);
 		$this->lang->load('auth');
 	}
